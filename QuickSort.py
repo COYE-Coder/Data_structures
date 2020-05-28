481bec2f3c53a818402bd07544f41ea7d8d66867
Sean Carter <47835300+COYE-Coder@users.noreply.github.com>
2020-05-28 17:30:11 -0600

    Add files via upload
    Still not done!

--- QuickSort.py
+++ QuickSort.py
@@ -5,18 +5,27 @@ import numpy as np
 
 
 def quicksort(arr):
+
 	arr_length = len(arr)
 
 
-	#Find median of last 3 elements in arr
-	last_three = np.array(arr[-3:])
+	if arr_length > 3:
+		#Find median of last n elements in arr
+		last_n = np.array(arr[-3:])
 
-	#Set median as pivot
-	pivot = int(np.percentile(last_three,50))
+		#Set median as pivot
+		pivot = int(np.percentile(last_n,50))
+		#print("Pivot = {}".format(pivot))
 
-	#Guarantee to find the "right-most" pivot index
-	pivot_index = arr.index(pivot,arr_length-3,arr_length)
-	
+		#Guarantee to find the "right-most" pivot index
+		pivot_index = arr.index(pivot,arr_length-3,arr_length)
+		
+
+	else:
+		pivot = arr[-1]
+		pivot_index = len(arr)-1
+
+
 	#Compare pivot to all elements to the left
 	#Stops when pivot has reached the same index that it will be evaluated against
 	i = 0
@@ -39,14 +48,27 @@ def quicksort(arr):
 
 			#Increment pivot index
 			pivot_index -= 1
-			print(pivot_index)
-			print(arr)
 
-		#Increment index
+		#Increment index- changes checked_element if pivot < c_e
 		i += 1
-	print(arr)
-		# After we guarantee that the pivot is > first element,
-		# we set the checked_element to be the next element
+
+
+	#Split array into two components above and below pivot
+	left_half = arr[:pivot_index]
+	right_half = arr[pivot_index:]
+
+
+	#If split concurs a small enough array, all pivots should be sorted
+
+	#Need to store original arr somewhere -- always returns None
+	if len(left_half) < 2 and len(right_half) < 2:
+		return arr
+
+	left_half_sorted = quicksort(left_half)
+	right_half_sorted = quicksort(right_half)
+	return left_half_sorted+right_half_sorted
+	
+
 
 
 
@@ -54,117 +76,10 @@ def quicksort(arr):
 
 
 # nested = np.random.randint(0,50,size=8).tolist()
-arr = [35, 39, 49, 17, 4, 0, 11, 9]
-print(arr)
-quicksort(arr)
-
-# arr_length = len(arr)
-# last_three = np.array(arr[-3:])
-# pivot = int(np.percentile(last_three,50))
-# pivot_index = arr.index(pivot,arr_length-3,arr_length)
-
-# i = 0
-# checked_element = arr[i]
-
-# print(checked_element > pivot)
-
-# left_of_p = arr[pivot_index-1]
-# print(left_of_p)
-
-# arr[pivot_index] = checked_element
-# print(arr)			
-# arr[i] = left_of_p
-# checked_element = arr[i]
-# print(arr)
-# arr[pivot_index-1] = pivot
-# print(arr)
-
-# pivot_index -= 1
-
-# print("//========================================================")
-
-# left_of_p = arr[pivot_index-1]
-# print(left_of_p)
-
-# arr[pivot_index] = checked_element
-# print(arr)			
-# arr[i] = left_of_p
-# print(arr)
-# arr[pivot_index-1] = pivot
-# checked_element = arr[i]
-
-# print(arr)
-
-# pivot_index -= 1
-
-
-# print(checked_element > pivot)
-
-# #Increment checked element
-# i+= 1
-# checked_element = arr[i]
-
-# print("//========================================================")
-# print(checked_element)
-# left_of_p = arr[pivot_index-1]
-# print(left_of_p)
-# arr[pivot_index] = checked_element
-# print(arr)
-# arr[i] = left_of_p
-# arr[pivot_index-1] = pivot
-# print(arr)
-# checked_element = arr[i]
-# pivot_index-=1
-
-# print(checked_element > pivot)
-
-# #Increment checked element
-# i+= 1
-# checked_element = arr[i]
-# print("//========================================================")
-# print(checked_element)
-
-
-# print(checked_element > pivot)
-
-# left_of_p = arr[pivot_index-1]
-# print(left_of_p)
-# arr[pivot_index] = checked_element
-# print(arr)
-# arr[i] = left_of_p
-# print(arr)
-# arr[pivot_index-1] = pivot
-# print(arr)
-# checked_element = arr[i]
-
-# pivot_index -= 1
-
-# print(checked_element > pivot)
-
-# print("//========================================================")
-# print("pivot_index: " + str(pivot_index))
-# left_of_p = arr[pivot_index-1]
-# print(left_of_p)
-
-# arr[pivot_index] = checked_element
-# print(arr)
-# arr[i] = left_of_p
-# print(arr)
-# arr[pivot_index-1] = pivot
-# print(arr)
-# checked_element = arr[i]
-
-# pivot_index -= 1
-# print(pivot_index)
-# print(i)
-
-# print(checked_element > pivot)
-
-
-
-
-
-
+arr = [0, 4, 34, 17, 11, 49, 35, 38]
+print(quicksort(arr))
+# print(nested)
+# print(quicksort(nested))
 
 
 
