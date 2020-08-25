c7c3b62b247386d283ff2523fc4da9797c7d762a
Sean Carter <47835300+COYE-Coder@users.noreply.github.com>
2020-08-25 15:41:12 -0600

    Updated to implement "Quick Union" Algorithm

--- UnionFind.py
+++ UnionFind.py
@@ -1,4 +1,4 @@
-# Naive Implementation of Union Find
+# Smarter Implementation of Union Find- uses the "Quick Union" algorithm
 
 class UnionFind:
     # First define a data structure
@@ -10,15 +10,18 @@ class UnionFind:
         self.d_structure = [i for i in range(size)]
 
     def is_connected(self, p, q):
-        return self.d_structure[p] == self.d_structure[q]
+        return self.get_root(p) == self.get_root[q]
 
-    # Note poor efficiency
+    def get_root(self, p):
+        while self.d_structure[p] != p:
+            p = self.d_structure[p]
+        return p
+
     def union(self, p, q):
-        old_p = self.d_structure[p]
-        old_q = self.d_structure[q]
-        for i in range(self.size):
-            if self.d_structure[i] == old_p:
-                self.d_structure[q] = old_p
+        root_p = self.get_root(p)
+        root_q = self.get_root(q)
+        if root_p != root_q:
+            self.d_structure[root_p] = self.d_structure[root_q]
 
     def display(self):
         print(self.d_structure)
